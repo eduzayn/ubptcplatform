21693c471eece33a96bd790a3f916cb2aed6bb19
eduzayn <ti@eduzayn.com.br>
2025-03-23 01:19:43 -0300

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { useAuth as useSupabaseAuth } from "@/lib/hooks";
-import { AuthProvider as SupabaseAuthProvider } from "@/lib/hooks/useAuth";
+import { AuthProvider, useAuth } from "@/lib/hooks/useAuth";
+
+export { AuthProvider, useAuth };
 
 type AuthWrapperProps = {
   children: React.ReactNode;
 };
 
 export const AuthWrapper = ({ children }: AuthWrapperProps) => {
-  return <SupabaseAuthProvider>{children}</SupabaseAuthProvider>;
+  return <AuthProvider>{children}</AuthProvider>;
 };
-
-// Re-exportar o hook useAuth para facilitar o uso
-export const useAuth = useSupabaseAuth;
-
-// Dummy export to fix TypeScript errors
-export const AuthProvider = AuthWrapper;
