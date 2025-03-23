9e7acfdddaec139761122029e07b1f511ba5f64d
eduzayn <ti@eduzayn.com.br>
2025-03-23 01:23:46 -0300

    Update useProfile.ts

--- src/lib/hooks/useProfile.ts
+++ src/lib/hooks/useProfile.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../supabase";
-import { useAuth } from "./useAuth";
+import { useAuth } from "@/context/AuthContext";
 import type { PostgrestError } from "@supabase/supabase-js";
 import type { Database } from "@/types/database.types";
 
